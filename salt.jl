13fc073225ce9ce8e136685e3e08f6ffb26885e0
David Liu <daveliu@Davids-Mac-mini.local>
2014-02-26 19:30:11 -0500

    added SlepcFinalize to salt.jl, and moved the int32 conversions into the ccall of Salt

--- salt.jl
+++ salt.jl
@@ -7,14 +7,14 @@ ccall((:SlepcInitialize, slepc), PetscErrorCode,
               C_NULL, C_NULL, C_NULL, C_NULL);
 
 
-N = int32([100, 1, 1]);
-Nc = int32(1);
-M = int32([50, 1, 1]);
-Npml = int32([20, 0, 0]);
-LowerPML = int32(0);
-bl = int32([1, -1, 1]);
-BCPeriod = int32(-1); nev = int32(1);
-printnewton = int32(1);
+N = [100, 1, 1];
+Nc = 1;
+M = [50, 1, 1];
+Npml = [20, 0, 0];
+LowerPML = 0;
+bl = [1, -1, 1];
+BCPeriod = -1; nev = 1;
+printnewton = 1;
 h = [.01, .1, .2];
 wa = 15.0;
 y = 3.0;
@@ -34,7 +34,13 @@ namesin = ["pass0", "pass1"];
 namesout = ["after0", "after1"];
 Nm = 2;
 
-ccall((:Salt, saltlib), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint}, Cint, Cint, Ptr{Uint8}, Ptr{Uint8}, Cdouble, Cdouble,
-Cint, Ptr{Cint}, Ptr{Cdouble}, Cdouble, Cdouble, Cdouble, Cint, Ptr{Uint8}, Cdouble, Cdouble, Cdouble, Cdouble, Ptr{Ptr{Uint8}}, Ptr{Ptr{Uint8}}, Cint, Cint), N, M, h, Npml, Nc, LowerPML, epsfile, fproffile, wa, y,
-	BCPeriod, bl, k, wreal, wimag, modenorm, nev, modeout,
-	dD, Dmax, thresholdw_tol, ftol, namesin, namesout, printnewton, Nm);
+ccall((:Salt, saltlib), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint}, 
+Cint, Cint, Ptr{Uint8}, Ptr{Uint8}, Cdouble, Cdouble,
+Cint, Ptr{Cint}, Ptr{Cdouble}, Cdouble, Cdouble, Cdouble, Cint, Ptr{Uint8}, 
+Cdouble, Cdouble, Cdouble, Cdouble, Ptr{Ptr{Uint8}}, Ptr{Ptr{Uint8}}, Cint, 
+Cint), int32(N), int32(M), h, int32(Npml), int32(Nc), int32(LowerPML), 
+epsfile, fproffile, wa, y, int32(BCPeriod), int32(bl), k, wreal, wimag, modenorm, 
+int32(nev), modeout, dD, Dmax, thresholdw_tol, ftol, namesin, namesout, 
+int32(printnewton), int32(Nm));
+
+ccall((:SlepcFinalize, slepc), PetscErrorCode, ());
