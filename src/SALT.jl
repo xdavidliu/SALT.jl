c7daea44b621ffdd8b182cb28399cc9303d10186
Steven G. Johnson <stevenj@alum.mit.edu>
2014-02-28 12:58:50 -0500

    Julia convention is types uppercase, functions lowercase, and usually vars lowercase

--- src/SALT.jl
+++ src/SALT.jl
@@ -30,9 +30,9 @@ type Geometry
     end
 end
 
-function Geometry(ε::Array{Cdouble}, h_, Npml_,
+function Geometry(ε::Array{Cdouble}, h_, nPML_,
                   gain_prof::Array{Cdouble}, ω_gain::Real, γ_gain::Real;
-                  Nc::Integer=3, LowerPML::Bool=true)
+                  nc::Integer=3, lowerPML::Bool=true)
     ndims(ε) > 3 && throw(ArgumentError("ε array must be <= 3-dimensional"))
     size(ε) != size(gain_prof) && throw(ArgumentError("gain profile must be same size as ε array"))
     N = Cint[size(ε)...]
@@ -44,16 +44,16 @@ function Geometry(ε::Array{Cdouble}, h_, Npml_,
     while length(h) < 3
         push!(h, h[end])
     end
-    Npml = Cint[Npml_...]
-    length(Npml) > 3 && throw(ArgumentError("Npml must have length <= 3"))
-    while length(Npml) < 3
-        push!(Npml, Npml[end])
+    nPML = Cint[nPML_...]
+    length(nPML) > 3 && throw(ArgumentError("nPML must have length <= 3"))
+    while length(nPML) < 3
+        push!(nPML, nPML[end])
     end
 
     Geometry(ccall(("CreateGeometry",saltlib), Geometry_,
                    (Ptr{Cint}, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint},
                     Cint, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cdouble, Cdouble),
-                   N, N, h, Npml, Nc, LowerPML, ε, gain_prof, ω_gain, γ_gain))
+                   N, N, h, nPML, nc, lowerPML, ε, gain_prof, ω_gain, γ_gain))
 end
 
 import Base.show
@@ -66,14 +66,14 @@ end
 
 #=========== master Salt function from C code ============ #
 
-Salt(N, Nc, M, Npml, LowerPML, bl, BCPeriod, printnewton, 
+Salt(N, nc, M, nPML, lowerPML, bl, BCPeriod, printnewton, 
 h, wa, y, wreal, wimag, k, modenorm, ftol, thresholdw_tol,
 dD, Dmax, eps, fprof, modeout, namesin, namesout, Nm) = 
 ccall((:Salt, saltlib), Void, (Ptr{Cint}, Ptr{Cint}, Ptr{Cdouble}, Ptr{Cint}, 
 Cint, Cint, Ptr{Cdouble}, Ptr{Cdouble}, Cdouble, Cdouble,
 Cint, Ptr{Cint}, Ptr{Cdouble}, Cdouble, Cdouble, Cdouble, Cint, Ptr{Uint8}, 
 Cdouble, Cdouble, Cdouble, Cdouble, Ptr{Ptr{Uint8}}, Ptr{Ptr{Uint8}}, Cint, 
-Cint), int32(N), int32(M), h, int32(Npml), int32(Nc), int32(LowerPML), 
+Cint), int32(N), int32(M), h, int32(nPML), int32(nc), int32(lowerPML), 
 eps, fprof, wa, y, int32(BCPeriod), int32(bl), k, wreal, wimag, modenorm, 
 int32(nev), modeout, dD, Dmax, thresholdw_tol, ftol, namesin, namesout, 
 int32(printnewton), int32(Nm));
@@ -81,20 +81,20 @@ int32(printnewton), int32(Nm));
 
 #============ sub-function for Passive resonance eigensolver ========== #
 
-Passive(N, Nc, M, Npml, LowerPML, bl, BCPeriod, 
+Passive(N, nc, M, nPML, lowerPML, bl, BCPeriod, 
 h, wreal, wimag, k, modenorm, eps, fprof, modeout) =
  
-Salt(N, Nc, M, Npml, LowerPML, bl, BCPeriod, 0, 
+Salt(N, nc, M, nPML, lowerPML, bl, BCPeriod, 0, 
 h, 0.0, 0.0, wreal, wimag, k, modenorm, 1.0e-7, 1.0e-7,
 0.0, 0.0, eps, fprof, modeout, [""], [""], 0);
 
 #============ sub-function for Newton solver ======= #
 
-Creeper(N, Nc, M, Npml, LowerPML, printnewton, 
+Creeper(N, nc, M, nPML, lowerPML, printnewton, 
 h, wa, y, ftol, thresholdw_tol,
 dD, Dmax, eps, fprof, namesin, namesout, Nm) =
 
-Salt(N, Nc, M, Npml, LowerPML, [0,0,0], 0, printnewton, 
+Salt(N, nc, M, nPML, lowerPML, [0,0,0], 0, printnewton, 
 h, wa, y, 0, 0, [0, 0, 0], 1.0, ftol, thresholdw_tol,
 dD, Dmax, eps, fprof, "", namesin, namesout, Nm);
 
