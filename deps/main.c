9248696a4efddc5236d1f311a01d8ec71637bfdf
David Liu <daveliu@Davids-Mac-mini.local>
2014-03-04 23:41:22 -0500

    separated Passive and Creeper into two separate functions in main.c

--- deps/main.c
+++ deps/main.c
@@ -1,8 +1,6 @@
-
 #include "salt.h"
 
 Mode *ReadModes(Geometry geo, char **namesin, char **namesout, int Nm){
-
 	Mode *ms;
 	int i;
 	for(i=0; i<Nm; i++){
@@ -19,20 +17,16 @@ Mode *ReadModes(Geometry geo, char **namesin, char **namesout, int Nm){
 }
 
 Geometry ReadCreateGeometry(){
-
-	
 	int i, N[3], Npml[3], Nc, LowerPML;
 	double h[3], wa, y;
-
 	char option[PETSC_MAX_PATH_LEN];
 	const char x[3] = {'x', 'y', 'z'};
-	for(i=0; i<3; i++){
 
+	for(i=0; i<3; i++){
 		sprintf(option, "%s%c", "-N", x[i]);
 		PetscOptionsGetInt(PETSC_NULL,option, &N[i], NULL);
 		sprintf(option, "%s%c", "-Npml", x[i]);
 		PetscOptionsGetInt(PETSC_NULL,option, &Npml[i], NULL);
-
 		sprintf(option, "%s%c", "-h", x[i]);
 		PetscOptionsGetReal(PETSC_NULL,option, &h[i], NULL);
 
@@ -49,7 +43,6 @@ Geometry ReadCreateGeometry(){
 	Vec veps, vfprof;
 	CreateVec(N[0]*N[1]*N[2], &veps);
 	VecDuplicate(veps, &vfprof);
-
 	FILE *fp;
 	fp = fopen(epsfile, "r");
 	if(fp==NULL){
@@ -79,146 +72,122 @@ Geometry ReadCreateGeometry(){
 	VecRestoreArray(vfprof, &fprof);
 	VecDestroy(&veps);
 	VecDestroy(&vfprof);
-
 	return geo;
 }
 
+void mainPassive(){
+
+	int i, BCPeriod=0, bl[3] = {0}, nev=0;
+	double k[3] = {0},wreal = 0., wimag=0., modenorm=1.;
+	char option[PETSC_MAX_PATH_LEN];
+	const char x[3] = {'x', 'y', 'z'};
+
+	for(i=0; i<3; i++){
+		sprintf(option, "%s%c", "-b", x[i]);
+		PetscOptionsGetInt(PETSC_NULL,option, &bl[i], NULL);
+
+		sprintf(option, "%s%c", "-k", x[i]);
+		PetscOptionsGetReal(PETSC_NULL,option, &k[i], NULL);
+	}
+	char modeout[PETSC_MAX_PATH_LEN] = "";
+	PetscOptionsGetReal(PETSC_NULL,"-norm", &modenorm,NULL); 
+	PetscOptionsGetReal(PETSC_NULL,"-wreal", &wreal,NULL); 
+	PetscOptionsGetReal(PETSC_NULL,"-wimag", &wimag,NULL); 
+	PetscOptionsGetInt(PETSC_NULL,"-nev", &nev,NULL);
+	PetscOptionsGetInt(PETSC_NULL,"-BCPeriod", &BCPeriod,NULL);
+	PetscOptionsGetString(PETSC_NULL,"-passiveout", modeout, PETSC_MAX_PATH_LEN, NULL); 
+
+	Geometry geo = ReadCreateGeometry();
+	
+	int added;
+	Mode *ms = Passive(&added, BCPeriod, bl, k, wreal, wimag, modenorm, nev, geo);
+	for(i = 0; i<added; i++){
+		if(added == 1)
+			sprintf(ms[i]->name, "%s", modeout);
+		else 
+			sprintf(ms[i]->name, "%s%i", modeout, i);
+
+		Write(ms[i], geo);
+		VecDestroy(&ms[i]->vpsi);
+		// no need to DestroyMode here because J and ksp not created
+	}
+	free(ms);
+	DestroyGeometry(geo);
+}
+
+void mainCreeper(double Dmax){
+	int i, ih, Nm = 0, printnewton = 0;
+	double dD = 0.0, thresholdw_tol=0., ftol = 0.0;
+
+	Geometry geo;
+	PetscOptionsGetReal(PETSC_NULL,"-thresholdw_tol", &thresholdw_tol,NULL); 
+	PetscOptionsGetReal(PETSC_NULL,"-dD", &dD,NULL); 
+	PetscOptionsGetReal(PETSC_NULL,"-newtonf_tol", &ftol,NULL); 
+
+	char optionin[PETSC_MAX_PATH_LEN] = "-in0",
+		 optionout[PETSC_MAX_PATH_LEN] = "-out0",
+		 name[PETSC_MAX_PATH_LEN], name2[PETSC_MAX_PATH_LEN],
+		 **namesin = NULL, **namesout = NULL;
+
+	i=0;
+	while(1){
+		PetscBool flg;
+		PetscOptionsGetString(PETSC_NULL,optionin, name, PETSC_MAX_PATH_LEN, &flg); 
+		if( !flg ) break;
+
+		PetscOptionsGetString(PETSC_NULL,optionout, name2, PETSC_MAX_PATH_LEN, &flg); 
+		if( !flg )
+			MyError("number of -out less than number of -in!");
+
+		if(i==0){
+			namesin = (char **) malloc( 1*sizeof(char*) );
+			namesout = (char **) malloc( 1*sizeof(char*) );
+		}else{
+			namesin = (char **) realloc( namesin, (i+1)*sizeof(char*) );
+			namesout = (char **) realloc( namesout, (i+1)*sizeof(char*) );
+		}
+
+		namesin[i] = (char *) malloc( PETSC_MAX_PATH_LEN * sizeof(char) );
+		namesout[i] = (char *) malloc( PETSC_MAX_PATH_LEN * sizeof(char) );
+		sprintf(namesin[i], "%s", name);
+		sprintf(namesout[i], "%s", name2);
+
+		i++;
+		sprintf(optionin, "-in%i", i);
+		sprintf(optionout, "-out%i", i);
+	}
+	Nm = i;
+	PetscOptionsGetInt(PETSC_NULL,"-printnewton", &printnewton,NULL);
+	geo = ReadCreateGeometry();
+	Mode *ms = ReadModes(geo, namesin, namesout, Nm);
+
+	// hack, so that SALT.jl can access Creeper using just a pointer to modes.
+	Creeper(dD, Dmax, thresholdw_tol, ftol, ms, printnewton, Nm, geo);    
+	for(ih=0; ih<Nm; ih++){
+		Write(ms[ih], geo);
+		VecDestroy(&(ms[ih]->vpsi) ); // J and ksp destroyed in Creeper
+		free(ms[ih]);
+	}
+	DestroyGeometry(geo);
+
+	for(i=0; i<Nm; i++){
+		free( namesin[i]);
+		free( namesout[i]);
+	}
+	free(namesin);
+	free(namesout);
+}
 
 int main(int argc, char** argv){ 
 	SlepcInitialize(&argc, &argv, PETSC_NULL, PETSC_NULL); 
-
-	int i, ih, bl[3] = {0}, 
-		BCPeriod=0, nev=0, Nm = 0, printnewton = 0;
-	double Dmax, wreal = 0., wimag=0., modenorm=1.,
-		k[3] = {0}, dD = 0.0, thresholdw_tol=0., ftol = 0.0;
-	
+	double Dmax;
 	PetscOptionsGetReal(PETSC_NULL,"-Dmax", &Dmax,NULL); 
 
-	char option[PETSC_MAX_PATH_LEN];
-	const char x[3] = {'x', 'y', 'z'};
-	for(i=0; i<3; i++){
-
-
-		if(Dmax == 0.0){
-			sprintf(option, "%s%c", "-b", x[i]);
-			PetscOptionsGetInt(PETSC_NULL,option, &bl[i], NULL);
-
-			sprintf(option, "%s%c", "-k", x[i]);
-			PetscOptionsGetReal(PETSC_NULL,option, &k[i], NULL);
-		}
-	}
-
-
 	if(Dmax == 0.0){ // Passive
-
-		Geometry geo;
-		char modeout[PETSC_MAX_PATH_LEN] = "";
-		PetscOptionsGetReal(PETSC_NULL,"-norm", &modenorm,NULL); 
-		PetscOptionsGetReal(PETSC_NULL,"-wreal", &wreal,NULL); 
-		PetscOptionsGetReal(PETSC_NULL,"-wimag", &wimag,NULL); 
-
-		PetscOptionsGetInt(PETSC_NULL,"-nev", &nev,NULL);
-		PetscOptionsGetInt(PETSC_NULL,"-BCPeriod", &BCPeriod,NULL);
-
-		PetscOptionsGetString(PETSC_NULL,"-passiveout", modeout, PETSC_MAX_PATH_LEN, NULL); 
-
-	    geo = ReadCreateGeometry();
-		int added;
-		Mode *ms = Passive(&added, BCPeriod, bl, k, wreal, wimag, modenorm, nev, geo);
-		for(i = 0; i<added; i++){
-			if(added == 1)
-				sprintf(ms[i]->name, "%s", modeout);
-			else 
-				sprintf(ms[i]->name, "%s%i", modeout, i);
-
-			Write(ms[i], geo);
-			VecDestroy(&ms[i]->vpsi);
-			// no need to DestroyMode here because J and ksp not created
-		}
-		free(ms);
-	    DestroyGeometry(geo);
-
+		mainPassive();
 	}else{ // Creeper
-		Geometry geo;
-		PetscOptionsGetReal(PETSC_NULL,"-thresholdw_tol", &thresholdw_tol,NULL); 
-		PetscOptionsGetReal(PETSC_NULL,"-dD", &dD,NULL); 
-		PetscOptionsGetReal(PETSC_NULL,"-newtonf_tol", &ftol,NULL); 
-
-		char optionin[PETSC_MAX_PATH_LEN] = "-in0",
-			 optionout[PETSC_MAX_PATH_LEN] = "-out0",
-			 name[PETSC_MAX_PATH_LEN], name2[PETSC_MAX_PATH_LEN],
-			**namesin = NULL, **namesout = NULL;
-
-		i=0;
-		while(1){
-			PetscBool flg;
-			PetscOptionsGetString(PETSC_NULL,optionin, name, PETSC_MAX_PATH_LEN, &flg); 
-
-			if( !flg ) break;
-
-			PetscOptionsGetString(PETSC_NULL,optionout, name2, PETSC_MAX_PATH_LEN, &flg); 
-			if( !flg )
-				MyError("number of -out less than number of -in!");
-
-			if(i==0){
-				namesin = (char **) malloc( 1*sizeof(char*) );
-				namesout = (char **) malloc( 1*sizeof(char*) );
-			}else{
-				namesin = (char **) realloc( namesin, (i+1)*sizeof(char*) );
-				namesout = (char **) realloc( namesout, (i+1)*sizeof(char*) );
-			}
-
-			namesin[i] = (char *) malloc( PETSC_MAX_PATH_LEN * sizeof(char) );
-			namesout[i] = (char *) malloc( PETSC_MAX_PATH_LEN * sizeof(char) );
-
-			sprintf(namesin[i], "%s", name);
-			sprintf(namesout[i], "%s", name2);
-
-			i++;
-
-			sprintf(optionin, "-in%i", i);
-			sprintf(optionout, "-out%i", i);
-		}
-		Nm = i;
-
-		PetscOptionsGetInt(PETSC_NULL,"-printnewton", &printnewton,NULL);
-
-	    geo = ReadCreateGeometry();
-
-		Mode *ms = ReadModes(geo, namesin, namesout, Nm);
-
-		// hack, so that SALT.jl can access Creeper using just a pointer to modes.
-        Creeper(dD, Dmax, thresholdw_tol, ftol, ms, printnewton, Nm, geo);    
-
-		for(ih=0; ih<Nm; ih++){
-			Write(ms[ih], geo);
-			VecDestroy(&(ms[ih]->vpsi) ); // J and ksp destroyed in Creeper
-			free(ms[ih]);
-		}
-	    DestroyGeometry(geo);
-
-
-		for(i=0; i<Nm; i++){
-			free( namesin[i]);
-			free( namesout[i]);
-		}
-		free(namesin);
-		free(namesout);
-
+		mainCreeper(Dmax);
 	}
-
-
-//============================================================
-
-
-
-/*
-	PetscPrintf(PETSC_COMM_WORLD, "\n");
-	PetscPrintf(PETSC_COMM_WORLD, "TODO: a whole bunch of TODOs in Salt.c related to first step of multimode\n");
-	PetscPrintf(PETSC_COMM_WORLD, "future todo: add artificial crashes to enforce all the assumptions I'm making. For example, crash if any file read fails.\n");
-	PetscPrintf(PETSC_COMM_WORLD, "future todo: Make sure all MyError crashes crash all processes the way CHKERRQ does\n");
-	PetscPrintf(PETSC_COMM_WORLD, "TODO: compare multimode calculation speed to single mode with twice the pixels\n");
-*/
 
 	SlepcFinalize();
 	return 0;
